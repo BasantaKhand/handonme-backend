7f14252367f660988b394c9fc56669281724211b
BasantaKhand <khandbasanta55@gmail.com>
2026-06-20 13:30:00 +0545

    Switch review creation route to protect middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const reviewController = require("../controllers/reviewController");
-const auth = require("../middleware/auth");
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/", auth, reviewController.createReview);
+router.post("/", protect, reviewController.createReview);
 router.get("/user/:userId", reviewController.getReviewsForUser);
 
 module.exports = router;
