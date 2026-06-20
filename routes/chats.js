61bc7652d3d78978589d976977f88b36c8537c80
BasantaKhand <khandbasanta55@gmail.com>
2026-06-20 13:00:00 +0545

    Switch chat routes to protect middleware

--- routes/chats.js
+++ routes/chats.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const chatController = require("../controllers/chatController");
-const auth = require("../middleware/auth");
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/", auth, chatController.getChats);
-router.post("/", auth, chatController.createChat);
-router.get("/:chatId/messages", auth, chatController.getMessages);
-router.post("/:chatId/messages", auth, chatController.sendMessage);
+router.get("/", protect, chatController.getChats);
+router.post("/", protect, chatController.createChat);
+router.get("/:chatId/messages", protect, chatController.getMessages);
+router.post("/:chatId/messages", protect, chatController.sendMessage);
 
 module.exports = router;
