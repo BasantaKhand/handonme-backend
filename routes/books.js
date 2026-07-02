9609e9dff861593ae935f827b672c11329ccad49
BasantaKhand <khandbasanta55@gmail.com>
2026-07-02 11:00:00 +0545

    Accept photo uploads on the book update route

--- routes/books.js
+++ routes/books.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get("/", bookController.getBooks);
 router.get("/:id", bookController.getBookById);
 router.post("/", protect, upload.array("photos", 5), bookController.createBook);
-router.put("/:id", protect, bookController.updateBook);
+router.put("/:id", protect, upload.array("photos", 5), bookController.updateBook);
 router.delete("/:id", protect, bookController.deleteBook);
 
 module.exports = router;
