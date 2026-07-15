a8dfa60172a2e10910d2021e58e44b300504d3db
BasantaKhand <khandbasanta55@gmail.com>
2026-07-15 10:20:00 +0545

    Add chat unread-count route

--- routes/chats.js
+++ routes/chats.js
@@ -5,6 +5,7 @@ const { protect } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", protect, chatsController.getMyChats);
+router.get("/unread-count", protect, chatsController.getUnreadCount);
 router.post("/", protect, chatsController.createChat);
 router.get("/:id", protect, chatsController.getChatById);
 router.get("/:id/messages", protect, chatsController.getMessages);
